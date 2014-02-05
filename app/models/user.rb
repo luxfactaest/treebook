7e31ee31507d37384650fac7a27a75471f983938
Adam Kelm <luxfactaest@gmail.com>
2014-02-05 14:41:16 -0500

    Fixed bug with user profile validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,15 @@ class User < ActiveRecord::Base
 
   validates :last_name, presence: true
 
-  validates :profile_name, presence: true, uniqueness: true, format: {
-    with: /a-zA-Z0-9_-/, message: 'Must be formatted correctly.'
-  }
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                             with: /\A[a-zA-Z0-9_-]+\Z/,
+                             message: 'Must be formatted correctly.'
+                           }
+has_many :statuses
 
-  # def full_name
-  # 	first_name + " " + last_name
+  def full_name
+    first_name + " " + last_name
+  end
 end
